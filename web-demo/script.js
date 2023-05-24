f9365141788163d1643f9d042f60516ac43fc567
nailen1 <45677256+nailen1@users.noreply.github.com>
2023-05-25 06:31:02 +0900

    Add files via upload

--- web-demo/script.js
+++ web-demo/script.js
@@ -1,25 +1,61 @@
-// 블록을 클릭했을 때 팝업창을 띄우는 함수
-function showPopup(blockId, blockText) {
-    const popupContent = document.querySelector('#popup-content');
-    popupContent.innerHTML = blockText;
+const blocks = document.querySelectorAll('.block');
 
-    const popup = document.querySelector('#popup');
-    popup.style.display = 'block';
-
-    const popupClose = document.querySelector('#popup-close');
-    popupClose.addEventListener('click', function () {
-        popup.style.display = 'none';
-    });
+function generateRandomHash() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let hash = '';
+    for (let i = 0; i < 12; i++) {
+        hash += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return hash;
 }
 
-// 각 블록을 클릭했을 때 팝업창을 띄우는 이벤트 핸들러 등록
-for (let i = 1; i <= 9; i++) {
-    const block = document.querySelector(`#block-${i}`);
-    const blockText = '{"Index": 1, "Timestamp": 1684087378.6354618, "Data": {"Name": "Daniel", "Service": "Naver"}, "Previous Hash": "a84d25bff8f4f42c817328813b2511c11775a10e8f99d342f28da79a33986c96", "Hash": "35216b9b88a0fb76d5e2fab2e7d65fc6729ca41feea685f08f58e72273f5b878"}';
+blocks.forEach((block) => {
+    const hash = generateRandomHash();
+    block.textContent = `${hash}... `;
+});
 
-    block.addEventListener('click', function () {
-        showPopup(`block-${i}`, blockText);
+const blockBox = document.querySelectorAll('.block-box');
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+const deleteBtn = document.querySelector('.delete-btn');
+
+blockBox.forEach((block) => {
+    block.addEventListener('click', () => {
+        overlay.style.display = 'block';
+        popup.style.display = 'block';
     });
+});
 
-}
+closeBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+    const blockInfo = `
+          Transaction ID: 0xabc123<br>
+          Timestamp: 2023-05-12 10:30:15<br>
+          Block Number: 1234<br>
+          Previous Hash: 0x789def<br>
+          Hash: 0x123abc<br><br>
+          Transaction Details:<br>
+          Data: ShinhanCard requested access to Daniel's personal information.<br>
+          Date: 2023-05-11<br>
+          Time: 10:30:00<br>
+          Transaction Fee: 0.001 ETH<br>
+          Payment Address: 0x456def<br><br>
+          ### 암호화되어 전송된 개인정보 ###<br>
+          Personal Information:<br>
+          Name: Daniel<br>
+          Birthdate: 1900-01-01<br>
+          Gender: Male<br>
+          Address: Seoul, South Korea<br>
+          Phone Number: 010-1234-5678<br>
+          Email: daniel@email.com<br>
+          ############################`;
 
+    const blockInfoElement = document.querySelector('.block-info');
+    blockInfoElement.innerHTML = blockInfo;
+
+});
+
+deleteBtn.addEventListener('click', () => {
+});
